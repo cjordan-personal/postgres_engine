e7da6588f06419fd8a4d15d1647fdf7861130126
cjordan-personal <79223901+cjordan-personal@users.noreply.github.com>
2022-05-11 16:03:08 -0400

    Delete auth__postgres_engine directory

--- auth__postgres_engine/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-
-with open(str(pathlib.Path(__file__).parent.resolve()) + "/creds.txt", "r") as f:
-    for line in f.readlines():
-        a_pairs = line.rstrip().split("=")
-        os.environ[a_pairs[0]] = a_pairs[1]
-f.close()
